893ba150b9ce1161679c2d82c1c1f56dc503c064
PeterSamehShafik <petosameh82@gmail.com>
2024-08-10 23:47:54 +0300

    finisihg 2

--- src/Components/Wishlist/Wishlist.jsx
+++ src/Components/Wishlist/Wishlist.jsx
@@ -8,18 +8,16 @@ import ErrorText from '../../common/Error.jsx';
 
 const WishlistPage = () => {
     const [wishlist, setWishlist] = useState('loading');
+    const [loading, setLoading] = useState(false);
 
     const fetchWishlist = async () => {
         try {
             const response = await axios.get('/wishlist');
-            console.log(typeof (response.data.data.wishlist))
             setWishlist(response.data.data.wishlist);
         } catch (error) {
             console.log(error)
             toast.error('Failed to fetch wishlist. Please try again later.');
             setWishlist('error')
-        } finally {
-            setWishlist(false)
         }
     };
     useEffect(() => {
@@ -28,17 +26,25 @@ const WishlistPage = () => {
 
     const removeFromWishlist = async (id) => {
         try {
-            await axios.delete(`/wishlist/${id}`);
-            setWishlist((prev) => prev.filter((item) => item._id !== id));
+            setLoading(true)
+            await axios.delete('/wishlist', {
+                data: {
+                    product: id 
+                }
+            });
+            await fetchWishlist()
             toast.success('Item removed from wishlist.');
         } catch (error) {
             toast.error('Failed to remove item. Please try again later.');
+        } finally {
+            setLoading(false)
         }
     };
 
 
 
     return <>
+        {loading && <LoadingScreen fullScreen={true} />}
         {wishlist === 'loading' ? <LoadingScreen /> :
             wishlist === 'error' ? <ErrorText handleRefresh={fetchWishlist} /> :
                 !wishlist?.length ? <ErrorText handleRefresh={fetchWishlist} text='No items in your wishlist.' /> :
@@ -48,20 +54,20 @@ const WishlistPage = () => {
                         </Typography>
                         {wishlist?.map((item) =>
                             <Box
-                                key={item.product._id}
+                                key={item._id}
                                 className="bg-gray-50 dark:bg-gray-700 rounded-lg p-4 mb-4 shadow-md"
                             >
                                 <Typography variant="h6" className="text-gray-900 dark:text-gray-200">
-                                    {item.product.name}
+                                    {item.name}
                                 </Typography>
                                 <Typography variant="body2" className="text-gray-600 dark:text-gray-400">
-                                    {item.product.description}
+                                    {item.description}
                                 </Typography>
                                 <Typography variant="body2" className="text-gray-500 dark:text-gray-500">
-                                    Category: {item.product.category}
+                                    Category: {item.category?.name}
                                 </Typography>
                                 <IconButton
-                                    onClick={() => removeFromWishlist(item.product._id)}
+                                    onClick={() => removeFromWishlist(item._id)}
                                     className="text-red-500 hover:text-red-600"
                                 >
                                     <MdDelete />
