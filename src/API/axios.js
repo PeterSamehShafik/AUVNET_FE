2c617618bd43b34c4c360d10b9859b4a2ca93855
PeterSamehShafik <petosameh82@gmail.com>
2024-08-17 03:45:42 +0300

    edit be_url

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     withCredentials: true,
-    baseURL: 'https://auvnet-internship-assessment-be-fofx.vercel.app/api'
+    baseURL: 'https://auvnet-internship-assessment-be-fofx.vercel.app/api' //internship-assessment-xi.vercel.app
 });
